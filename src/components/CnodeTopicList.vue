06b734814fc42262790fb458685bced80d18e08b
wangxf <470992100@qq.com>
2016-11-14 17:38:50 +0800

    add filters

--- src/components/CnodeTopicList.vue
+++ src/components/CnodeTopicList.vue
@@ -78,6 +78,7 @@
 
 <script>
     import api from '../store/api.js'
+    import filters from '../filters'
     export default{
         data(){
             return {
@@ -91,44 +92,10 @@
         },
         filters:{
             tab(val,isTop,isGood){
-                if(isTop) return '置顶'
-                if(isGood) return '精华'
-                let tab = {
-                    'ask':'问答',
-                    'good':'精华',
-                    'job':'招聘',
-                    'share':'分享'
-                }
-                return tab[val]||'未知'
+               return filters.tab(val,isTop,isGood)
             },
             timeAgo(val){
-                let nowDate = new Date();
-                let replyDate = new Date(val);
-                let diffSeconds = (nowDate.getTime()-Number(replyDate.getTime()))/1000;
-                let years = 365*24*60*60;
-                let months = 30*24*60*60;
-                let days = 24*60*60;
-                let hours =  60*60;
-                let minutes = 60;
-                let seconds = 1;
-                if(diffSeconds<seconds){
-                    return '1秒以前'
-                }else if(seconds<=diffSeconds&&diffSeconds<minutes){
-                    return Math.floor(diffSeconds/seconds)+'秒前'
-                }else if(minutes<=diffSeconds&&diffSeconds<hours){
-                    return Math.floor(diffSeconds/minutes)+'分钟前'
-                }else if(hours<diffSeconds&&diffSeconds<days){
-                    return Math.floor(diffSeconds/hours)+'小时前'
-                }else if(days<diffSeconds&&diffSeconds<months){
-                    return Math.floor(diffSeconds/days)+'天前'
-                }else if(months<diffSeconds&&diffSeconds<years){
-                    return Math.floor(diffSeconds/months)+'个月前'
-                }else{
-                    return Math.floor(diffSeconds/years)+'年前'
-                }
-            },
-            getLastReplyUser(val){
-                
+                return filters.timeAgo(val)
             }
         },
         computed:{
--- src/views/CnodeDetail.vue
+++ src/views/CnodeDetail.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-    {{detail.content}}
+        <div v-if="isLoading">
+            {{detail.content}}
+        </div>
+        <div class="msg" v-else>
+            {{loadingMsg}}
+        </div>
     </div>
 </template>
 
 <style lang="stylus" scoped>
-    
+    .msg
+        width 100%
+        margin 30px auto
+        text-align center
 </style>
 
 
@@ -14,7 +22,9 @@ import api from '../store/api.js'
 export default {
     data(){
         return {
-            detail:{}
+            detail:{},
+            isLoading:false,
+            loadingMsg:'加载中..'
         }
     },
     methods:{
@@ -25,21 +35,29 @@ export default {
             })
             .then((res)=>{
                 let result = res.data;
+                this.isLoading = true;
+                this.loadingMsg = '';
                 if(result.success){
                     this.detail=result.data;
                 }
             })
             .catch((err)=>{
                 console.log(err);
+                this.isLoading = false;
+                this.loadingMsg = '加载数据失败'
             })
         }
     },
     watch:{
         $route(){
+            this.isLoading = false;
+            this.loadingMsg='加载中..'
             this.fetch();
         }
     },
     created(){
+        this.isLoading = false;
+        this.loadingMsg='加载中..'
         this.fetch()
     }
 }    
