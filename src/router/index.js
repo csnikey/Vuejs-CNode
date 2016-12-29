17dd1e0b1ccd23f66e1c3746b72171db5cdefe9d
wangxf <470992100@qq.com>
2016-12-30 03:04:07 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ export default new VueRouter({
   mode: 'history',
   routes: [
       {   
-          path: '/(Vuejs-CNode(\/index.html)?)?',redirect: '/topics/all'
+          path: '/\/index.html)?',redirect: '/topics/all'
       },
       {
           path: '/topics/:tab',component: CnodeTopicList
